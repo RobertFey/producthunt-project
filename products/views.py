8816488c75c8c97bd77574dc3b84487a4b46bbff
Robert Fey <r.a.g.fey@gmail.com>
2019-07-05 17:51:13 +0200

    Created the detail page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 
 
@@ -24,8 +24,13 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/' + str(product.id))
         else:
             return render(request, 'products/create.html', {'error':'All fields are required!'})
     else:
         return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product':product})
